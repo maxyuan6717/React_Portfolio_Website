4809d4db9d62e8d2e239f7abcbccbfacf97ef76a
Max Yuan <maxyuan6717@gmail.com>
2020-12-22 18:54:27 -0500

    Update link hover effect

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,8 +3,7 @@ export const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    --blue-color: ${({ theme }) => theme.blue_color};
-    --highlight-color: ${({ theme }) => theme.highlight_color};
+    --blue-color: ${({ theme }) => theme.blue_color};    
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
     transition: all 0.50s linear;
   }
