eee53cf6da481771d9da62c8790e9d13e1f412ba
Max Yuan <maxyuan6717@gmail.com>
2021-06-04 23:29:19 -0400

    extra surface

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,10 +1,10 @@
 export const lightTheme = {
-  surface: ["#FFF", "#e1e1e1"],
+  surface: ["#FFF", "#e1e1e1", "#FFF"],
   text: ["#363537", "#646464"],
   blue_color: "#3273dc",
 };
 export const darkTheme = {
-  surface: ["#121212", "#323232"],
+  surface: ["#121212", "#323232", "#404040"],
   text: ["#FAFAFA", "#c8c8c8"],
   blue_color: "#75a3eb",
 };
